3a9d05cf135c555bf6ad0f9fdfde95bc6209c773
Harishu1998 <harish19@iastate.edu>
2023-07-10 00:15:01 -0500

    changes in env

--- gym_examples/envs/grid_world.py
+++ gym_examples/envs/grid_world.py
@@ -2,157 +2,177 @@ import numpy as np
 import gymnasium
 from gymnasium import spaces
 import math
-import pandas as pd
 
 
 class GridWorldEnv(gymnasium.Env):
 
     def __init__(self):
-        self.t0 = 0.0  # initial time 
-        self.tf = 120  # final time minutes
-        self.dt = 0.1  # time step
-        self.observation_space  = spaces.Box(low=np.array([0]), high = np.array([20]), dtype=np.float64)
-        self.action_space = spaces.Box(low = np.array([0]), high=np.array([2]), dtype=np.int16) 
+        self.observation_space  = spaces.Box(low=np.array([0]), high = np.array([100]), dtype=np.float64)
+        self.action_space = spaces.Box(low = np.array([0]), high=np.array([2]), dtype=np.int16)
+
+       # Initial conditions
         self.X0 = 0.3e5  # cell/ml
         self.S0 = 0.2 # g/L
-        self.E0 = 0 # U/L 
+        self.E0 = 0.0 # U/L 
+        # Process conditions
+        self.T = np.random.randint(low=32, high=35) #'C
+        self.A = 250 # RPM
+
+        # Substrate addition : add 0.05 evert 24 hours
+        self.sadd = 0.05
+        self.sadd_t = 5 # add every 5 hours
+
+        #model parameters
+        self.Ks = 0.1
+        self.C = 0.000001
+
+        self.MuE = .00000001
+
+        self.MuD = 0
+
+        self.del_t = 0.1
+        self.sadd_s = self.sadd_t/self.del_t
+        self.t_end = 24*7
+        self.tvec = np.arange(0, self.t_end, self.del_t)
+        self.ns = len(self.tvec)
+
+        # X S E delE delX
+        self.D = np.zeros((self.ns+1, 5))
+
+        self.D[0][0] = self.X0
+        self.D[0][1] = self.S0
+        self.D[0][2] = self.E0
         self.i = 0
 
-        # Model Parameters
-        self.Ks = 0.1 #g/L substrate saturation coefficient
-        self.C = 0.000001 # ug/cell - glucose consumption per new cell created (growth coefficient)
-        self.MuX = 0.1 # 1/hr
-        self.MuE = .000001 # U/(cell*hr)
-        self.MuD = 0 # 1/hr
-        self.mu_opt = 1.8
-        self.T_opt = 37
-        self.A_opt = 250
-        self.r_t = 3.12
-        self.r_a = 4.5
-        self.temperature = np.random.randint(low=32, high=35)
-
-        # Substrate Values
-        self.S = np.zeros(int(self.tf/self.dt)+1)
-        # Initial Substrate
-        self.S[0] = self.S0
-
-        # Cell Values
-        self.X = np.zeros(int(self.tf/self.dt)+1)
-        # initial Cell concentration value
-        self.X[0] = self.X0
-
-        # Enzyme concentration
-        self.E0 = 0
-        self.E = np.zeros(int(self.tf/self.dt)+1)
-        self.E[0] = self.E0
-
-        # Time steps
-        self.t = np.arange(self.t0, self.tf+self.dt, self.dt)
-
-        # Change in Enzyme 
-        self.E_C = np.zeros(int(self.tf/self.dt))
-
-       
-        
+        self.media = 10 # Liters
+        self.max_capacity = 50 # Liters
+        self.media_added_in_liters = self.sadd/self.S0
 
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
-        self.t0 = 0.0  # initial time
-        self.tf = 120  # final time
-        self.dt = 0.1  # time step
-        self.i = 0
+       # Initial conditions
         self.X0 = 0.3e5  # cell/ml
         self.S0 = 0.2 # g/L
-        self.E0 = 0 # U/L 
+        self.E0 = 0.0 # U/L 
         # Process conditions
+        self.T = np.random.randint(low=32, high=35) #'C
+        self.A = 250 # RPM
 
-        # Model Parameters
-        self.Ks = 0.1 #g/L substrate saturation coefficient
-        self.C = 0.000001 # ug/cell - glucose consumption per new cell created (growth coefficient)
-        self.MuX = 0.1 # 1/hr
-        self.MuE = .000001 # U/(cell*hr)
-        self.MuD = 0 # 1/hr
-        self.mu_opt = 1.8
-        self.T_opt = 37
-        self.A_opt = 250
-        self.r_t = 3.12
-        self.r_a = 4.5
-        self.temperature = np.random.randint(low=32, high=35)
+        # Substrate addition : add 0.05 evert 24 hours
+        self.sadd = 0.05
+        self.sadd_t = 5 # add every 5 hours
 
-        # Substrate Values
-        self.S = np.zeros(int(self.tf/self.dt)+1)
-        # Initial Substrate
-        self.S[0] = self.S0
+        #model parameters
+        self.Ks = 0.1
+        self.C = 0.000001
 
-        # Cell Values
-        self.X = np.zeros(int(self.tf/self.dt)+1)
-        # initial Cell concentration value
-        self.X[0] = self.X0
+        self.MuE = .00000001
 
-        # Enzyme concentration
-        self.E0 = 0
-        self.E = np.zeros(int(self.tf/self.dt)+1)
-        self.E[0] = self.E0
+        self.MuD = 0
 
-        # Time steps
-        self.t = np.arange(self.t0, self.tf+self.dt, self.dt)
+        self.del_t = 0.1
+        self.sadd_s = self.sadd_t/self.del_t
+        self.t_end = 24*7
+        self.tvec = np.arange(0, self.t_end, self.del_t)
+        self.ns = len(self.tvec)
 
-        # Change in Enzyme 
-        self.E_C = np.zeros(int(self.tf/self.dt))
+        # X S E delE delX
+        self.D = np.zeros((self.ns+1, 5))
 
-        # Reset makes the enzyme concetration 0
+        self.D[0][0] = self.X0
+        self.D[0][1] = self.S0
+        self.D[0][2] = self.E0
+
+        self.media = 10 # Liters
+        
+        # Reset makes    the enzyme concetration 0
         observation = np.array([self.E0],dtype=float)
         info = {}
-
+        self.i = 0
         with open("plot.txt",'w') as f:
             f.truncate(0)
-        return observation, info
-
+        return observation,info
 
     def step(self, action):
-        # Termincation condition
-        if self.i == 1199:
+        if self.i == int(self.t_end/self.del_t):
             terminate = True
-            return np.array([self.E[self.i]]), 0, terminate, False, {} 
+            return np.array([self.D[self.i][2]]), 0, terminate, False, {}
+        elif self.media >= self.max_capacity:
+            terminate = True
+            return np.array([self.D[self.i][2]]), 0, terminate, False, {}
         else:
             terminate = False
             action = math.ceil(action[0])
-            initial_cordinates = [self.i, self.E[self.i]]  
+            initial_cordinates = [self.i, self.D[self.i][2]]  
             if action == 2:
-                self.temperature -= 0.1
+                self.T -= 0.1
             else:
-                self.temperature += action/10
+                self.T += action/10
 
-            MuX =  self.mu_opt*(math.exp(-((self.temperature - self.T_opt)**2)/self.r_t**2))
+            MuX =  0.1*(math.exp(-((self.T - 37)**2)/5**2))
+    
+            dXdt = (MuX *self.D[self.i][1]) / (self.Ks + self.D[self.i][1]) * self.D[self.i][0]
 
-            dXdt = (MuX *self.S[self.i]) / (self.Ks + self.S[self.i]) * self.X[self.i]
+            dSdt = ( -self.C * MuX*self.D[self.i][1]/(self.Ks+self.D[self.i][1]) * self.D[self.i][0] ) 
+
+            delX = dXdt * self.del_t
+            delS = dSdt * self.del_t
+
+            # Change in cells
+            self.D[self.i+1][4] = delX
+
+            # time of cell death 
+            tcd = 48
+            tcd_s = math.floor(tcd/self.del_t)
+            cellsub = 0
+
+            if self.i+1 == tcd_s:
+                cellsub = self.X0 # seeded cells die on this step
+
+            elif self.i+1 > tcd_s :
+                cellsub = self.D[(self.i+1)-tcd][4]
+
+            # Update cells
+            self.D[self.i+1][0] = self.D[self.i][0] + delX - cellsub
+
+            # Update substrate 
+            if self.i % self.sadd_s == 0:
+                delS = delS + self.sadd
+                self.media += self.media_added_in_liters
             
-            dSdt = ( -self.C * MuX*self.S[self.i]/(self.Ks + self.S[self.i]) * self.X[self.i] )
+            self.D[self.i+1][1] = self.D[self.i][1] + delS
 
-            delX = dXdt * self.dt
-            delS = dSdt * self.dt
+            # Enzyme determination 
 
-            self.X[self.i+1] = self.X[self.i] + delX
-            self.S[self.i+1] = self.S[self.i] + delS
-
-            delE = self.MuE * dXdt * self.dt
+            delE = self.MuE * dXdt*self.del_t # new enzyme from fresh cells
 
             step = self.i
             nts = 1
 
-            while step > 0 and nts < 500:
-                delE = delE + (self.X[self.i+1] - self.X[self.i]) * (self.MuE)*(1/nts)
-                nts += 1
-                step -= 1
+            t_ev = 20 # hours cells produce enzyme with linear depletion
+            s_ev = 20/self.del_t # Timesteps cells can make enzyme
+
+            for j in range(0,int(s_ev)):
+                ts = self.i - j
+                if ts < 1 :
+                    break
+                else:
+                    delE = delE + (- (self.MuE/s_ev)*j+self.MuE) * self.D[ts][4]
+
+            # Change in enzyme    
+            self.D[self.i+1][3] = delE
             
-            self.E_C[self.i] = delE
-            self.E[self.i+1] = self.E[self.i] +  delE
+            #Update enzyme variable
+            self.D[self.i+1][2] = self.D[self.i][2] + delE
 
-            if self.i+1 > 51:
-                self.E[self.i+1] = self.E[self.i+1] - self.E_C[self.i-50]
+            # Degradation of enzymes
+            EDT = 72
 
-            final_cordinates = [self.i+1, self.E[self.i+1]]
+            if self.i+1>(EDT/self.del_t+1):
+                self.D[self.i+1][2] = self.D[self.i+1][2] - self.D[self.i+1 - int(EDT/self.del_t)][3]
+            
+            final_cordinates = [self.i+1, self.D[self.i+1][2]]
             
             slope = (final_cordinates[1] - initial_cordinates[1]) / (final_cordinates[0] - initial_cordinates[0])
 
@@ -164,21 +184,7 @@ class GridWorldEnv(gymnasium.Env):
                 reward = 10 + slope
 
             with open('plot.txt','a') as plotting_file:
-                plotting_file.write(f"{self.i},{self.temperature}\n")
+                plotting_file.write(f"{self.tvec[self.i]},{self.D[self.i][0]/1e6},{self.D[self.i][1]},{self.D[self.i][2]}\n")
             self.i += 1
-            data = {
-            'experiment_iteration': self.i,
-            'cells': self.X[self.i],
-            'substrate': self.S[self.i],
-            'Enzyme' : self.E[self.i]
-            }
-            df = pd.DataFrame(data)
-            df.to_csv("output_logs.csv",mode='a', index=True)
-            return np.array([self.E[self.i]]), reward, terminate, False, {}
-
-    def render(self):
-        pass
-
-    
-    def close(self):
-        pass
+            
+            return np.array([self.D[self.i][2]]), reward, terminate, False, {}
